ee903aa175e253959deb607f344527cdf78d1b6f
Konboi <ryosuke.yabuki@gmail.com>
2024-02-07 15:39:59 +0900

    rename package from com.theokanning to com.launchableinc

---
+++ api/src/main/java/com/launchableinc/openai/DeleteResult.java
@@ -0,0 +1,25 @@
+package com.launchableinc.openai;
+
+import lombok.Data;
+
+/**
+ * A response when deleting an object
+ */
+@Data
+public class DeleteResult {
+
+	/**
+	 * The id of the object.
+	 */
+	String id;
+
+	/**
+	 * The type of object deleted, for example "file" or "model"
+	 */
+	String object;
+
+	/**
+	 * True if successfully deleted
+	 */
+	boolean deleted;
+}
---
+++ api/src/main/java/com/launchableinc/openai/billing/Plan.java
@@ -0,0 +1,13 @@
+package com.launchableinc.openai.billing;
+
+import lombok.Data;
+
+/**
+ *
+ */
+@Data
+public class Plan {
+
+	private String title;
+	private String id;
+}
